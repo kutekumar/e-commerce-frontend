c3cd5348d7d2b8c51cdbf5694e88e5ce77757c6c
kutekumar <kutekumar@gmail.com>
2021-05-10 20:17:54 +0630

    Implementing HomeScreen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
@@ -8,18 +9,24 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Kumar's Online Shop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Kumar's Online Shop </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='m-auto'>
-              <Nav.Link href='/cart'>
-                <ShoppingCartIcon />
-                Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <AccountCircleIcon />
-                Sign In
-              </Nav.Link>
+            <Nav style={{ marginLeft: "auto" }}>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <ShoppingCartIcon />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <AccountCircleIcon />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
